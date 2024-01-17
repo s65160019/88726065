e9892f03a2f569279377c5d31f9318dcad6fe936
s65160019 <65160019@go.buu.ac.th>
2024-01-17 19:03:05 +0700

    'ลบรายการtodo'

--- lab07/scripts.js
+++ lab07/scripts.js
@@ -19,6 +19,10 @@ document.addEventListener("DOMContentLoaded", function () {
     }
     }
     // ลบรายการ Todo
+    function deleteTodo(index) {
+        todos.splice(index, 1);
+        renderTodoList();
+    }
     // ตรวจสอบ/ยกเลิกการเสร็จสิ้นรายการ Todo
     function toggleComplete(index) {
     todos[index].completed = !todos[index].completed;
