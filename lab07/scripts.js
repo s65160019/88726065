06aa791b6c8e2d67b11800cdf93a457e2edb9757
s65160019 <65160019@go.buu.ac.th>
2024-01-17 19:04:52 +0700

    'ลบใหม่อีกรอบ'

--- lab07/scripts.js
+++ lab07/scripts.js
@@ -24,10 +24,6 @@ document.addEventListener("DOMContentLoaded", function () {
         renderTodoList();
     }
     // ตรวจสอบ/ยกเลิกการเสร็จสิ้นรายการ Todo
-    function toggleComplete(index) {
-    todos[index].completed = !todos[index].completed;
-    renderTodoList();
-    }
     // แสดงรายการ Todo บนหน้าเว็บ
     function renderTodoList() {
     console.log(todos);
